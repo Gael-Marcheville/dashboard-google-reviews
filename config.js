0b8936575af9dcc4d47f72aef7897b58399255d5
Gael-Marcheville <gael.marcheville@telecom-paris.fr>
2024-04-25 11:23:14 +0200

    try another url

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
     starsDisplayedFilter: [1, 2, 3, 4, 5],
     isAnsweredFilter: [1, 1],
     default_client_id: '',
-    url_server: 'app-gaelmarcheville.koyeb.app/proxy',
+    url_server: '/proxy',
     url_website: 'app-gaelmarcheville.koyeb.app/'
 };
